2874371aae8810ce329ed17f1ceb268e910debe8
CynthiaVl <cynthia.vanlaer@gmail.com>
2024-08-12 12:03:07 +0200

    tickets component + routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { TicketsComponent } from './tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, ContactComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, ContactComponent, TicketsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { TicketsComponent } from './tickets/tickets.component';
 
 export const routes: Routes =
 [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'tickets', component: TicketsComponent },
 ];
